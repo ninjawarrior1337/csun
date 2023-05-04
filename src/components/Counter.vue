52156c3b79f76f233a81bdcb9f8223fb1f7c9f22
ninjawarrior1337 <me@treelar.xyz>
2023-05-03 17:29:34 -0700

    this is dumb

---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex flex-row items-center justify-evenly align-middle">
+        <button class="rounded-full text-4xl bg-muse aspect-square w-16" @click="count--">
+            -
+        </button>
+        <p class="text-5xl font-bold">{{ count }}</p>
+        <button class="rounded-full text-4xl bg-muse aspect-square w-16" @click="count++">
+            +
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+import confetti from 'canvas-confetti';
+
+const count = ref(0)
+
+const launchConfetti = (count: number) => {
+    confetti({
+            colors: ["#3399ff", "e4007f"],
+            shapes: ["circle", "square", "star"],
+            particleCount: count,
+            spread: 360
+    })
+}
+
+watch(count, (c) => {
+    if(Number.isInteger(Math.log10(c))) {
+        launchConfetti(c)
+    }
+})
+</script>
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+		  colors: {
+			muse: "#e4007f",
+			aqours: "#009fe8",
+			niji: "#fab920",
+			treelar: "#3399ff"
+		  },
+		},
+	},
+	plugins: [
+		require("@tailwindcss/typography")
+	],
+}
