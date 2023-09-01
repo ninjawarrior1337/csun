e4f65bfb9cb07d76e991a7ac18decefab0a028f6
ninjawarrior1337 <me@treelar.xyz>
2023-08-31 23:57:47 -0700

    add first project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind(), vue(), mdx()],
   base: "/~tr807414/main"
 });
